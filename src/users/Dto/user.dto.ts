6ae3bfb82fc724813456024766814b4e75588d42
sudo-abhinav <presonalabhi@gmail.com>
2024-06-26 21:39:26 +0530

    creating Post route with prisma ORM

---
+++ src/users/Dto/user.dto.ts
@@ -0,0 +1,37 @@
+/* eslint-disable prettier/prettier */
+import { IsAlphanumeric, IsEmail, IsNotEmpty, IsString } from "class-validator"
+
+
+// ! now i am adding class-validator & calss-transformer from nest pipes so we can add validation 
+// ! so i have to convert interface to class
+
+export class Authdto{ 
+
+    @IsString()
+    @IsNotEmpty()
+    firstName :string
+
+    // ---------------
+    @IsString()
+    @IsNotEmpty()
+    lasName?:string
+// -----------
+
+    @IsEmail()
+    @IsNotEmpty()
+    email:string
+
+    @IsAlphanumeric()
+    @IsNotEmpty()
+    password:string   
+}
+
+
+// ! thses pipes are not woking until we dont declare in main file we made pipes global for nestJS
+
+
+
+
+
+
+
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,20 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { PrismaService } from 'src/prisma/Prisma.service';
 
 @Injectable()
 export class UsersService {
-
+constructor(private prisma:PrismaService){}
     data():string{
         return 'data is called'
+    }
+
+    async CreateUser(){
+        const userData = await this.prisma.userSignup.create({
+            data:{
+                firstName:
+            },
+        })
     }
 }
